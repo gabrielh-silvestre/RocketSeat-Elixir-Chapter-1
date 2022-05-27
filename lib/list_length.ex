937c23ced32ba08e6b3278150ef5c6c118ead862
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-05-27 19:35:09 -0300

    feat: complete challenge

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: my_length(list, 0)
+
+  defp my_length([], acc), do: acc
+
+  defp my_length([_head | tail], acc) do
+    acc = acc + 1
+    my_length(tail, acc)
   end
+
+  # def call(list), do: length(list)
 end
